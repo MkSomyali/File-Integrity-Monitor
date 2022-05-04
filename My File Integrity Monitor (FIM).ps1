32519adeff500daa5951682f7de336e82ab10636
MK_ <102767906+MkSomyali@users.noreply.github.com>
2022-05-05 00:06:45 +0200

    Add files via upload

---
+++ My File Integrity Monitor (FIM).ps1
@@ -0,0 +1,98 @@
+﻿
+
+Write-Host ""
+Write-Host "What would you like to do?"
+Write-Host
+Write-Host "  A) Collect new Baseline?"
+Write-Host "  B) Begin monitorisng files with saved Baseline?"
+Write-Host ""
+$response = Read-Host -Prompt "Please enter 'A' or 'B'"
+
+Function Calculate-File-Hash($filepath) {
+         $filehash = Get-FileHash -Path $filepath -Algorithm SHA512
+         return $filehash
+}
+
+Function Erase-Baseline-If-Already-Exists() {
+         $baselineExists = Test-Path .\baseline.txt
+
+     if ($baselineExists) {
+#Delete it
+        Remove-Item .\baseline.txt
+    }
+}
+
+     if ($response -eq "A".ToUpper()) {
+#Delete baseline.txt if it already exists
+       Erase-Baseline-If-Already-Exists
+
+#Calculate Hash from the target files and store in baseline.txt
+
+#Collect all the files in the target folder
+       $files = Get-ChildItem -Path .\Files
+
+#For each File, calculae the hash, and write to baseline.txt
+       foreach ($f in $files) {
+            $hash = Calculate-File-Hash $f.Fullname
+           "$($hash.Path)|$($hash.Hash)" | Out-File .\baseline.txt -Append
+            }
+}
+elseif ($response -eq "B".ToUpper()) {
+
+        $fileHashDictionary = @{}
+
+#Load file|hash from baseline.txt and store them in a dictionary
+        $filePathHashes = Get-Content -Path .\baseline.txt
+   
+       foreach ($f in $filePathHashes) {
+              $fileHashDictionary.add($f.Split("|")[0],$f.Split("|")[1])
+            }
+
+# Begin (continuously) monitoring files with saved Baseline
+       Write-Host "Read existing baseline.txt, start monitoring files." -ForegroundColor Green
+       while ($true) {
+       Start-Sleep -Seconds 1
+  
+            $files = Get-ChildItem -Path .\Files
+
+#For each File, calculae the hash, and write to baseline.txt
+        foreach ($f in $files) {
+             $hash = Calculate-File-Hash $f.Fullname
+#"$($hash.Path)|$($hash.Hash)" | Out-File .\baseline.txt -Append
+            
+#Notify if a new file has been created
+            if ($fileHashDictionary[$hash.Path] -eq $null) {
+#A new file has been created!
+                 Write-Host "$($hash.Path) has been created!" -ForegroundColor Cyan
+            }
+            else {
+
+#Notify if a new file has been changed
+            if ($fileHashDictionary[$hash.Path] -eq $hash.Hash) {
+            #The file has not changed
+            }
+            else {
+#File file has been compromised!, notify the user
+                 Write-Host "$($hash.Path) has changed!!!" -ForegroundColor DarkRed
+                 }
+
+            }
+            
+         }
+
+         foreach ($key in $fileHashDictionary.Keys) {
+                $baselineFileStillExists = Test-Path $key
+             if (-Not $baselineFileStillExists) {
+#One of the baseline files must have been deleted, notify the user
+                  Write-Host "$($key) has been deleted!" -ForegroundColor DarkMagenta
+                 }
+
+            }
+
+        }
+}
+#Refine/ clean
+#Send Notifications to phone/email & text
+#error checking
+#Specific foldermonitoring
+
